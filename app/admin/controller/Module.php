dc561313eee3f94384cda42b9bbb546e823a6e5e
lande <939134342@qq.com>
2025-05-14 22:23:07 +0800

    feat:插件市场开发

--- app/admin/controller/Module.php
+++ app/admin/controller/Module.php
@@ -6,4 +6,41 @@ use app\common\controller\Backend;
 
 class Module extends Backend
 {
+    public function index()
+    {
+        if ($this->isAjax()) {
+            $list = [
+                [
+                    "id" => 1,
+                    "image" => "https://gw.alipayobjects.com/zos/rmsportal/gLaIAoVWTtLbBWZNYEMg.png",
+                    "title" => "Alipay",
+                    "remark" => "那是一种内在的东西， 他们到达不了，也无法触及的",
+                    "time" => "几秒前"
+                ],
+                [
+                    "id" => 1,
+                    "image" => "https://gw.alipayobjects.com/zos/rmsportal/gLaIAoVWTtLbBWZNYEMg.png",
+                    "title" => "Alipay",
+                    "remark" => "那是一种内在的东西， 他们到达不了，也无法触及的",
+                    "time" => "几秒前"
+                ],
+                [
+                    "id" => 1,
+                    "image" => "https://gw.alipayobjects.com/zos/rmsportal/gLaIAoVWTtLbBWZNYEMg.png",
+                    "title" => "Alipay",
+                    "remark" => "那是一种内在的东西， 他们到达不了，也无法触及的",
+                    "time" => "几秒前"
+                ],
+                [
+                    "id" => 1,
+                    "image" => "https://gw.alipayobjects.com/zos/rmsportal/gLaIAoVWTtLbBWZNYEMg.png",
+                    "title" => "Alipay",
+                    "remark" => "那是一种内在的东西， 他们到达不了，也无法触及的",
+                    "time" => "几秒前"
+                ]
+            ];
+            $this->success('ok', null, $list);
+        }
+        return $this->view->fetch();
+    }
 }
