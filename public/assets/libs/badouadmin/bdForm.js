9cb60b26dca7896aa2a1248d44ac10b12dddae8b
YuriWu <877732602@qq.com>
2025-06-14 20:55:32 +0800

    feat:更新管理员

--- public/assets/libs/badouadmin/bdForm.js
+++ public/assets/libs/badouadmin/bdForm.js
@@ -242,9 +242,13 @@ layui.define(['jquery', 'bdHttp', 'form', 'iconPicker', 'toast', 'bdUpload', 'co
             },
             //这是一个函数，接收四个参数：form: 表单元素或表单的选择器。success: 成功回调函数。error: 错误回调函数。submit: 提交回调函数。
             bindevent: function (form, success, error, submit) {
+                // console.log("success");
+
+                
+
                 //如果传入的 form 不是对象，则将其转换为 jQuery 对象。这确保了后续操作可以直接使用 jQuery 方法。
                 form = typeof form === 'object' ? form : $(form);
-                console.log('表单数据');
+                console.log('表单数据对象-id');
                 console.log(form);
                 //从 bdForm 对象中获取 events 对象，它包含了一些与表单相关的事件处理方法。
                 var events = bdForm.events;
