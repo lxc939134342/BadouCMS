32b51e3e3269942bb51f724004e5bb97f214ad9c
wu <958342972@qq.com>
2025-06-18 19:49:43 +0800

    fix:更新后台用户修改功能

--- app/admin/model/User.php
+++ app/admin/model/User.php
@@ -48,15 +48,38 @@ class User extends Model
         return $value ? cdnurl($value, true) : letter_avatar($data['nickname']);
     }
 
+    public function setBirthdayAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    public function setPrevtimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
     public function getPrevtimeTextAttr($value, $data)
     {
         $value = $value ? $value : ($data['prevtime'] ?? "");
         return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
     }
 
+    public function setLogintimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
     public function getLogintimeTextAttr($value, $data)
     {
         $value = $value ? $value : ($data['logintime'] ?? "");
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+    public function setJointimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+    public function getJointimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : ($data['jointime'] ?? "");
         return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
     }
 
