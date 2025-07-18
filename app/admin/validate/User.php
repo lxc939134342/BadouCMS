b06bc5e192b85d158da6f6dfa1fc89d37c1f6752
wu <958342972@qq.com>
2025-07-18 20:52:36 +0800

    fix:增加验证

---
+++ app/admin/validate/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\admin\validate;
+
+use think\Validate;
+
+class User extends Validate
+{
+
+    protected $failException = true;
+
+    /**
+     * 验证规则
+     */
+    protected $rule = [
+        'username'    => 'require|regex:^[a-zA-Z][a-zA-Z0-9_]{2,15}$|unique:user',
+        'nickname' => 'require|unique:user',
+        'email'       => 'email|unique:user',
+        'mobile'      => 'mobile|unique:user',
+        'password'    => 'require|regex:^(?!.*[&<>"\'\n\r]).{6,32}$',
+    ];
+
+    /**
+     * 字段描述
+     */
+    protected $field = [
+    ];
+    /**
+     * 提示消息
+     */
+    protected $message = [
+    ];
+    /**
+     * 验证场景
+     */
+    protected $scene = [
+        'add'  => ['username', 'nickname', 'password', 'email', 'mobile'],
+        'edit' => ['username', 'nickname', 'password', 'email', 'mobile'],
+    ];
+
+    public function __construct(array $rules = [], $message = [], $field = [])
+    {
+        $this->field = [
+            'username' => __('Username'),
+            'nickname' => __('Nickname'),
+            'password' => __('Password'),
+            'email'    => __('Email'),
+            'mobile'   => __('Mobile')
+        ];
+        parent::__construct($rules, $message, $field);
+    }
+
+}
