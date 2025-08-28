631cd36c849ac8fb6fcbc6044b7058a1080f57db
lande <939134342@qq.com>
2025-08-28 12:35:32 +0800

    更新版本号2.0.8

--- config/badouadmin.php
+++ config/badouadmin.php
@@ -66,6 +66,6 @@ return [
     'backup_global_files'   => true,
     // 升级时忽略升级的目录
     'upgrade_ignore_dirs' => ['template/cms/default/'],
-    'version' => 'v2.0.7',
+    'version' => 'v2.0.8',
     'api_url' => 'https://sq.badoucms.com/api/',
 ];
