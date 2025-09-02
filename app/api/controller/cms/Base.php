3a2f9dc4875e593db6783be72562971b5f977a59
lande <939134342@qq.com>
2025-09-02 19:03:39 +0800

    fix:修复cms API强制认证 无效的问题

--- app/api/controller/cms/Base.php
+++ app/api/controller/cms/Base.php
@@ -27,37 +27,42 @@ class Base extends Api
     public function checkAccess()
     {
         $config = get_sys_config();
-        // 判断用户
-        if (!$config['api_appid']) {
-            $this->error('请求失败：管理后台接口认证用户配置有误', '', 403);
+        if (! isset($config['api_open']) || ! $config['api_open']) {
+            $this->error('系统尚未开启API功能，请到后台配置', '', 403);
         }
-        // 判断密钥
-        if (!$config['api_secret']) {
-            $this->error('请求失败：管理后台接口认证密钥配置有误', '', 403);
-        }
-        $appid = $this->request->param('appid') ? $this->request->param('appid') : $this->request->header('appid');
-        $timestamp = $this->request->param('timestamp') ? $this->request->param('timestamp') : $this->request->header('timestamp');
-        $signature = $this->request->param('signature') ? $this->request->param('signature') : $this->request->header('signature');
-        // 获取参数
-        if (!$appid) {
-            $this->error('请求失败：未检查到appid参数', '', 403);
-        }
-        if (!$timestamp) {
-            $this->error('请求失败：未检查到timestamp参数', '', 403);
-        }
-        if (!$signature) {
-            $this->error('请求失败：未检查到signature参数', '', 403);
-        }
+        if ($config['api_auth']) {
+            // 判断用户
+            if (!$config['api_appid']) {
+                $this->error('请求失败：管理后台接口认证用户配置有误', '', 403);
+            }
+            // 判断密钥
+            if (!$config['api_secret']) {
+                $this->error('请求失败：管理后台接口认证密钥配置有误', '', 403);
+            }
+            $appid = $this->request->param('appid') ? $this->request->param('appid') : $this->request->header('appid');
+            $timestamp = $this->request->param('timestamp') ? $this->request->param('timestamp') : $this->request->header('timestamp');
+            $signature = $this->request->param('signature') ? $this->request->param('signature') : $this->request->header('signature');
+            // 获取参数
+            if (!$appid) {
+                $this->error('请求失败：未检查到appid参数', '', 403);
+            }
+            if (!$timestamp) {
+                $this->error('请求失败：未检查到timestamp参数', '', 403);
+            }
+            if (!$signature) {
+                $this->error('请求失败：未检查到signature参数', '', 403);
+            }
 
-        $host = request()->scheme()."://".request()->host(true);
-        // 验证时间戳
-        if (strpos($_SERVER['HTTP_REFERER'], $host) === false && time() - $timestamp > 15) { // 请求时间戳认证，不得超过15秒
-            $this->error('请求失败：接口时间戳验证失败！', '', 403);
-        }
+            $host = request()->scheme()."://".request()->host(true);
+            // 验证时间戳
+            if (strpos($_SERVER['HTTP_REFERER'], $host) === false && time() - $timestamp > 15) { // 请求时间戳认证，不得超过15秒
+                $this->error('请求失败：接口时间戳验证失败！', '', 403);
+            }
 
-        // 验证签名
-        if ($signature != md5(md5($config['api_appid'] . $config['api_secret'] . $timestamp))) {
-            $this->error('请求失败：接口签名信息错误！', '', 403);
+            // 验证签名
+            if ($signature != md5(md5($config['api_appid'] . $config['api_secret'] . $timestamp))) {
+                $this->error('请求失败：接口签名信息错误！', '', 403);
+            }
         }
     }
 
