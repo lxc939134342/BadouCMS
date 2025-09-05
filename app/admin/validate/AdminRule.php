ccaf7ad17b33677fb375a11f3748d534aca48bd8
lande <939134342@qq.com>
2025-09-05 16:07:14 +0800

    fix:优化菜单规则

--- app/admin/validate/AdminRule.php
+++ app/admin/validate/AdminRule.php
@@ -9,7 +9,7 @@ class AdminRule extends Validate
     /**
      * 正则
      */
-    protected $regex = ['format' => '[a-z0-9_\/]+'];
+    protected $regex = ['format' => '[a-z0-9_\/.]+'];
 
     /**
      * 验证规则
@@ -23,7 +23,7 @@ class AdminRule extends Validate
      * 提示消息
      */
     protected $message = [
-        'name.format' => 'URL规则只能是小写字母、数字、下划线和/组成'
+        'name.format' => 'URL规则只能是小写字母、数字、下划线和/.组成'
     ];
 
     protected $scene = [
