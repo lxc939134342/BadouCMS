13eabb560c20c159eb59aa3c57d5aa89cd9d038d
lande <939134342@qq.com>
2025-09-06 21:53:10 +0800

    feat:cms扩展字段增加多图标题类型

--- app/admin/model/cms/Extfield.php
+++ app/admin/model/cms/Extfield.php
@@ -57,7 +57,8 @@ class Extfield extends Model
             '6'  => ['text' => '附件上传', 'inputType' => 'files', 'type' => 'string', 'limit' => 255, 'default' => ''],
             '7'  => ['text' => '日期选择', 'inputType' => 'datetime', 'type' => 'datetime', 'limit' => 0, 'default' => null],
             '8'  => ['text' => '编辑器', 'inputType' => 'editor', 'type' => 'text', 'limit' => 0, 'default' => null],
-            '9'  => ['text' => '下拉选择', 'inputType' => 'select', 'type' => 'string', 'limit' => 255, 'default' => '']
+            '9'  => ['text' => '下拉选择', 'inputType' => 'select', 'type' => 'string', 'limit' => 255, 'default' => ''],
+            '11'  => ['text' => '多图标题(只增加字段)', 'inputType' => 'images_title', 'type' => 'string', 'limit' => 1000, 'default' => '']
         ];
 
         return $options;
