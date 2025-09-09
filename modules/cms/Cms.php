6ee617fc2b5c3c270d07c0a50f7d75f31e37fb11
lande <939134342@qq.com>
2025-09-09 08:02:00 +0800

    fix:优化单独安装 cms 时导入配置信息

--- modules/cms/Cms.php
+++ modules/cms/Cms.php
@@ -3,6 +3,7 @@
 namespace modules\cms;
 
 use app\common\library\Menu;
+use app\admin\model\Config as ConfigModel;
 
 class Cms
 {
@@ -28,12 +29,25 @@ class Cms
     {
         $menu = include_once __DIR__ . '/menu.php';
         Menu::create($menu);
+
+        $config = include_once __DIR__ . '/config.php';
+        $configModel = new ConfigModel();
+        $configModel->setGroup('cms', 'CMS配置');
+        $configModel->setSysConfig($config, false);
+        $configModel::clear();
+
         return true;
     }
 
     public function uninstall()
     {
         Menu::delete('cms');
+
+        $configModel = new ConfigModel();
+        $configModel->delGroup('cms');
+        $configModel->where('group', 'cms')->delete();
+        $configModel::clear();
+
         return true;
     }
 
