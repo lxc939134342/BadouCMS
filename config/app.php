b95b2d11651642c2eab80b3f2bc1bb90606539cc
lande <939134342@qq.com>
2025-09-19 10:11:49 +0800

    fix:优化异常处理

--- config/app.php
+++ config/app.php
@@ -31,10 +31,10 @@ return [
     ],
 
     // 异常页面的模板文件
-    'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
+    'exception_tmpl'   => app()->getRootPath() . 'app'.DIRECTORY_SEPARATOR.'common'.DIRECTORY_SEPARATOR.'view'.DIRECTORY_SEPARATOR.'tpl'.DIRECTORY_SEPARATOR.'think_exception.tpl',
 
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'   => true,
+    'show_error_msg'   => env('APP_DEBUG', 'true'),
 ];
