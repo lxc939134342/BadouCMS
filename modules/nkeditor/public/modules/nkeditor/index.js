422930c954cb6e1f31d763cca8dea36bad955e96
lande <939134342@qq.com>
2025-10-18 08:44:10 +0800

    fix:优化富文本加载

--- modules/nkeditor/public/modules/nkeditor/index.js
+++ modules/nkeditor/public/modules/nkeditor/index.js
@@ -161,140 +161,144 @@ KindEditor.ready(function (K) {
             };
         });
 
-        K.create(".editor", {
-            dialogOffset: 0, //对话框距离页面顶部的位置，默认为0居中，
-            allowFileManager: true,
-            allowImageUpload: true,
-            allowMediaUpload: true,
-            filePostName: "file", // 确保上传文件的表单名称为 'file'
-            uploadJson: badou.api.fixurl("ajax/upload"), // 保持此配置，供其他插件使用
-            fileManagerJson: badou.api.fixurl("general.attachment/select?element_id=&multiple=true&mimetype=*"), // 文件管理器功能保持不变
-            themeType: "black", //主题
-            width: "100%",
-            items: [
-                "source",
-                "undo",
-                "redo",
-                "preview",
-                "print",
-                "template",
-                "code",
-                "quote",
-                "cut",
-                "copy",
-                "paste",
-                "plainpaste",
-                "wordpaste",
-                "justifyleft",
-                "justifycenter",
-                "justifyright",
-                "justifyfull",
-                "insertorderedlist",
-                "insertunorderedlist",
-                "indent",
-                "outdent",
-                "subscript",
-                "superscript",
-                "clearhtml",
-                "quickformat",
-                "selectall",
-                "formatblock",
-                "fontname",
-                "fontsize",
-                "forecolor",
-                "hilitecolor",
-                "bold",
-                "italic",
-                "underline",
-                "strikethrough",
-                "lineheight",
-                "removeformat",
-                "image",
-                "multiimage",
-                "graft",
-                "media",
-                "insertfile",
-                "table",
-                "hr",
-                "emoticons",
-                "baidumap",
-                "pagebreak",
-                "anchor",
-                "link",
-                "unlink",
-                "about",
-                "fullscreen",
-            ],
-            noDisableItems: ["source", "fullscreen"], // 确保这些项在禁用模式下仍然可用
-            afterCreate: function () {
-                var self = this;
-                //Ctrl+回车提交
-                K.ctrl(document, 13, function () {
-                    self.sync(); // 同步编辑器内容到 textarea
-                    // 确保 self.srcElement[0] 存在且是有效的 DOM 元素
-                    if (self.srcElement && self.srcElement[0]) {
-                        $(self.srcElement[0]).closest("form").submit();
-                    } else {
-                        console.warn("KindEditor: srcElement[0] is undefined, cannot submit form.");
-                    }
-                });
-                K.ctrl(self.edit.doc, 13, function () {
-                    self.sync(); // 同步编辑器内容到 textarea
-                    // 确保 self.srcElement[0] 存在且是有效的 DOM 元素
-                    if (self.srcElement && self.srcElement[0]) {
-                        $(self.srcElement[0]).closest("form").submit();
-                    } else {
-                        console.warn("KindEditor: srcElement[0] is undefined, cannot submit form.");
-                    }
-                });
-
-                //粘贴上传
-                $("body", self.edit.doc).bind("paste", function (event) {
-                    var image, pasteEvent;
-                    pasteEvent = event.originalEvent;
-                    if (pasteEvent.clipboardData && pasteEvent.clipboardData.items) {
-                        image = getImageFromClipboard(pasteEvent);
-                        if (image) {
-                            event.preventDefault();
-                            // 调用统一的上传函数处理粘贴的图片
-                            upload(self, image, function (data) {
-                                self.exec("insertimage", data.data.url);
-                            });
+        var _bindevent = layui.bdForm.events.bindevent;
+        layui.bdForm.events.bindevent = function (form) {
+            _bindevent.apply(this, [form]);
+            K.create(".editor", {
+                dialogOffset: 0, //对话框距离页面顶部的位置，默认为0居中，
+                allowFileManager: true,
+                allowImageUpload: true,
+                allowMediaUpload: true,
+                filePostName: "file", // 确保上传文件的表单名称为 'file'
+                uploadJson: badou.api.fixurl("ajax/upload"), // 保持此配置，供其他插件使用
+                fileManagerJson: badou.api.fixurl("general.attachment/select?element_id=&multiple=true&mimetype=*"), // 文件管理器功能保持不变
+                themeType: "black", //主题
+                width: "100%",
+                items: [
+                    "source",
+                    "undo",
+                    "redo",
+                    "preview",
+                    "print",
+                    "template",
+                    "code",
+                    "quote",
+                    "cut",
+                    "copy",
+                    "paste",
+                    "plainpaste",
+                    "wordpaste",
+                    "justifyleft",
+                    "justifycenter",
+                    "justifyright",
+                    "justifyfull",
+                    "insertorderedlist",
+                    "insertunorderedlist",
+                    "indent",
+                    "outdent",
+                    "subscript",
+                    "superscript",
+                    "clearhtml",
+                    "quickformat",
+                    "selectall",
+                    "formatblock",
+                    "fontname",
+                    "fontsize",
+                    "forecolor",
+                    "hilitecolor",
+                    "bold",
+                    "italic",
+                    "underline",
+                    "strikethrough",
+                    "lineheight",
+                    "removeformat",
+                    "image",
+                    "multiimage",
+                    "graft",
+                    "media",
+                    "insertfile",
+                    "table",
+                    "hr",
+                    "emoticons",
+                    "baidumap",
+                    "pagebreak",
+                    "anchor",
+                    "link",
+                    "unlink",
+                    "about",
+                    "fullscreen",
+                ],
+                noDisableItems: ["source", "fullscreen"], // 确保这些项在禁用模式下仍然可用
+                afterCreate: function () {
+                    var self = this;
+                    //Ctrl+回车提交
+                    K.ctrl(document, 13, function () {
+                        self.sync(); // 同步编辑器内容到 textarea
+                        // 确保 self.srcElement[0] 存在且是有效的 DOM 元素
+                        if (self.srcElement && self.srcElement[0]) {
+                            $(self.srcElement[0]).closest("form").submit();
+                        } else {
+                            console.warn("KindEditor: srcElement[0] is undefined, cannot submit form.");
                         }
-                    }
-                });
-                //拖拽上传
-                $("body", self.edit.doc).bind("drop", function (event) {
-                    var images, pasteEvent;
-                    pasteEvent = event.originalEvent;
-                    if (pasteEvent.dataTransfer && pasteEvent.dataTransfer.files) {
-                        images = getImagesFromDrop(pasteEvent);
-                        if (images.length > 0) {
-                            event.preventDefault();
-                            $.each(images, function (i, image) {
-                                // 调用统一的上传函数处理每个拖拽的图片
+                    });
+                    K.ctrl(self.edit.doc, 13, function () {
+                        self.sync(); // 同步编辑器内容到 textarea
+                        // 确保 self.srcElement[0] 存在且是有效的 DOM 元素
+                        if (self.srcElement && self.srcElement[0]) {
+                            $(self.srcElement[0]).closest("form").submit();
+                        } else {
+                            console.warn("KindEditor: srcElement[0] is undefined, cannot submit form.");
+                        }
+                    });
+
+                    //粘贴上传
+                    $("body", self.edit.doc).bind("paste", function (event) {
+                        var image, pasteEvent;
+                        pasteEvent = event.originalEvent;
+                        if (pasteEvent.clipboardData && pasteEvent.clipboardData.items) {
+                            image = getImageFromClipboard(pasteEvent);
+                            if (image) {
+                                event.preventDefault();
+                                // 调用统一的上传函数处理粘贴的图片
                                 upload(self, image, function (data) {
                                     self.exec("insertimage", data.data.url);
                                 });
-                            });
+                            }
                         }
+                    });
+                    //拖拽上传
+                    $("body", self.edit.doc).bind("drop", function (event) {
+                        var images, pasteEvent;
+                        pasteEvent = event.originalEvent;
+                        if (pasteEvent.dataTransfer && pasteEvent.dataTransfer.files) {
+                            images = getImagesFromDrop(pasteEvent);
+                            if (images.length > 0) {
+                                event.preventDefault();
+                                $.each(images, function (i, image) {
+                                    // 调用统一的上传函数处理每个拖拽的图片
+                                    upload(self, image, function (data) {
+                                        self.exec("insertimage", data.data.url);
+                                    });
+                                });
+                            }
+                        }
+                    });
+                },
+                beforeUpload: function (ke_upload_button_callback, file) {
+                    var self = this; // 'this' 指向 KindEditor 实例
+                    var file = file ? file : $("input.ke-upload-file", this.form).prop("files")[0];
+                    upload(self, file, ke_upload_button_callback);
+                    return false; // 阻止 KindEditor 内部上传按钮的默认提交，由我们的 AJAX 处理
+                },
+                //错误处理 handler
+                errorMsgHandler: function (message, type) {
+                    try {
+                        JDialog.msg({ type: type, content: message, timer: 2000 });
+                    } catch (Error) {
+                        layer.msg(message, { icon: 2, time: 2000 });
                     }
-                });
-            },
-            beforeUpload: function (ke_upload_button_callback, file) {
-                var self = this; // 'this' 指向 KindEditor 实例
-                var file = file ? file : $("input.ke-upload-file", this.form).prop("files")[0];
-                upload(self, file, ke_upload_button_callback);
-                return false; // 阻止 KindEditor 内部上传按钮的默认提交，由我们的 AJAX 处理
-            },
-            //错误处理 handler
-            errorMsgHandler: function (message, type) {
-                try {
-                    JDialog.msg({ type: type, content: message, timer: 2000 });
-                } catch (Error) {
-                    layer.msg(message, { icon: 2, time: 2000 });
-                }
-            },
-        });
+                },
+            });
+        }
     });
 });
