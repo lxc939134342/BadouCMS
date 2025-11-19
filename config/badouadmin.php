95c732aaeef8ca46710c928ec84388a6b8ac128b
lande <939134342@qq.com>
2025-11-19 19:55:57 +0800

    feat:发行为 v2.1.3

--- config/badouadmin.php
+++ config/badouadmin.php
@@ -66,6 +66,6 @@ return [
     'backup_global_files'   => true,
     // 升级时忽略升级的目录
     'upgrade_ignore_dirs' => ['template/cms/default/'],
-    'version' => 'v2.1.2',
+    'version' => 'v2.1.3',
     'api_url' => 'https://sq.badoucms.com/api/',
 ];
