7dc222ceef006d696769a34c17cc3dae1e0cfa1c
优选传媒-0731.Top <cfphp@foxmail.com>
2026-04-20 03:41:04 +0000

    update app/admin/lang/zh-cn/cms/area.php. 增加对区域管理的中文提示
    Signed-off-by: 优选传媒-0731.Top <cfphp@foxmail.com>

--- app/admin/lang/zh-cn/cms/area.php
+++ app/admin/lang/zh-cn/cms/area.php
@@ -5,4 +5,10 @@ return [
     'Acode' => '区域代码',
     'Domain' => '域名',
     'Is default' => '是否默认',
+    'The Chinese area is not allowed to be deleted' => '中文区域不允许删除',
+    'The default region is not allowed for deletion' => '默认区域不允许删除',
+    'Chinese area cannot be deleted when there are no other areas' => '当没有其他区域时，中文区域不能删除',
+    'Cannot delete area with existing categories' => '无法删除有栏目存在的区域',
+    'Cannot delete area with existing content' => '无法删除有内容存在的区域',
+    'Cannot delete area that is currently enabled' => '无法删除处于开启状态的区域',
 ];
